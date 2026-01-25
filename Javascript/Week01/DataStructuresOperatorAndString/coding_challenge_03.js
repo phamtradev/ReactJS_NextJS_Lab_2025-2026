87d733f7c9220d12cfc06592843b384d28cb16ae
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-25 13:18:18 +0700

    feat(Week01): JavaScript_DataStructuresOperatorAndString_CodingChallenge03

---
+++ Javascript/Week01/DataStructuresOperatorAndString/coding_challenge_03.js
@@ -0,0 +1,26 @@
+const gameEvents = new Map([
+  [17, "⚽ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🟨 Yellow card"],
+  [69, "🟥 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽ GOAL"],
+  [80, "⚽ GOAL"],
+  [92, "🟨 Yellow card"],
+]);
+
+const events = [...new Set(gameEvents.values())];
+
+gameEvents.delete(64);
+
+console.log(
+  "An event happened, on average, every " + 90 / gameEvents.size + " minutes"
+);
+
+for (const [minute, event] of gameEvents) {
+  const half = minute <= 45 ? "FIRST HALF" : "SECOND HALF";
+  console.log("[" + half + "] " + minute + ": " + event);
+}
