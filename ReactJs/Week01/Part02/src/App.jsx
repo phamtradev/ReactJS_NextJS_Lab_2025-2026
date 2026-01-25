543461b93a3014203de7f0d8e3406aae0d830c9a
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-25 13:46:12 +0700

    feat(Week1): ReactJS_Part02

---
+++ ReactJs/Week01/Part02/src/App.jsx
@@ -0,0 +1,15 @@
+import Button from "./components/Button/Button";
+
+function App() {
+  return (
+    <>
+      <div className="app">
+        <Button type="primary">Primary</Button>
+        <Button type="danger">Danger</Button>
+        <Button type="success">Success</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ ReactJs/Week01/Part02/src/components/Button/Button.jsx
@@ -0,0 +1,7 @@
+import "./Button.css";
+
+const Button = ({ type, children }) => {
+  return <button className={`btn btn-${type}`}>{children}</button>;
+};
+
+export default Button;
