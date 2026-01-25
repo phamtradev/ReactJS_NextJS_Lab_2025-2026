4ece91303eef648deeffab0ba148dae37384d9e7
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-25 13:05:02 +0700

    feat(Week01): JavaScript_Part02_CodingChallenge04

---
+++ Javascript/Week01/Part02/coding_challenge_04.js
@@ -0,0 +1,31 @@
+const calcTip = (bill) => {
+  if (bill >= 50 && bill <= 300) {
+    return bill * 0.15;
+  } else {
+    return bill * 0.2;
+  }
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(bills[i] + tip);
+}
+
+const calcAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  return sum / arr.length;
+};
+
+console.log(bills);
+console.log(tips);
+console.log(totals);
+console.log(calcAverage(totals));
