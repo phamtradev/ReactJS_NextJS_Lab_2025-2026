82ee4c9ccaa935a8d6f96ef9ba003b4a42fe65ed
Phạm Trà <phamvantra.dev@gmail.com>
2026-01-25 13:03:34 +0700

    feat(Week01): JavaScript_Part02_CodingChallenge03

---
+++ Javascript/Week01/Part02/coding_challenge_03.js
@@ -0,0 +1,46 @@
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (john.bmi > mark.bmi) {
+  console.log(
+    john.fullName +
+      "'s BMI (" +
+      john.bmi.toFixed(1) +
+      ") is higher than " +
+      mark.fullName +
+      "'s (" +
+      mark.bmi.toFixed(1) +
+      ")!"
+  );
+} else {
+  console.log(
+    mark.fullName +
+      "'s BMI (" +
+      mark.bmi.toFixed(1) +
+      ") is higher than " +
+      john.fullName +
+      "'s (" +
+      john.bmi.toFixed(1) +
+      ")!"
+  );
+}
