4a9939b26a41a505c05990c49c98731d588c1603
H82M19\Student <Student>
2026-03-21 13:41:02 +0700

    feat(Week04): midterm_fetchData

--- ReactJs/Week04/mid-term/src/App.jsx
+++ ReactJs/Week04/mid-term/src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [data, setData] = useState([] );
+
+  const jsonData = "/data.json";
+
+  useEffect(() => {
+    async function fetchData() {
+      var res = await fetch(jsonData);
+      var data = await res.json();
+      console.log(data);
+      setData(data);
+    }
+    fetchData();
+  }, [])
 
   return (
     <>
-      
+      {
+        data.map((item) => (
+          <div key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.desc}</p>
+          </div>
+        ))
+      }
     </>
   )
 }
