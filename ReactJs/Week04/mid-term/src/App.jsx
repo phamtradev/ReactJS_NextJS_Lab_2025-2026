3112b478c3a9b5a7bbd6b0a055ac4787d4b0ed27
Phạm Trà <phamvantra.dev@gmail.com>
2026-03-21 23:14:07 +0700

    update: mid-term

--- ReactJs/Week04/mid-term/src/App.jsx
+++ ReactJs/Week04/mid-term/src/App.jsx
@@ -1,34 +1,71 @@
-import { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useMemo, useState } from "react";
+import "./App.css";
 
 function App() {
-  
-  const [data, setData] = useState([] );
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
 
   const jsonData = "/data.json";
 
   useEffect(() => {
     async function fetchData() {
-      var res = await fetch(jsonData);
-      var data = await res.json();
-      console.log(data);
+      const res = await fetch(jsonData);
+      const data = await res.json();
       setData(data);
     }
     fetchData();
-  }, [])
+  }, []);
+
+  const categories = useMemo(() => {
+    return [...new Set(data.map((item) => item.category))];
+  }, [data]);
+
+  const filteredData = useMemo(() => {
+    return data.filter((item) => {
+      const matchSearch = item.name
+        .toLowerCase()
+        .includes(searchValue.toLowerCase());
+
+      const matchCategory =
+        !selectedCategory || item.category === selectedCategory;
+
+      return matchSearch && matchCategory;
+    });
+  }, [data, searchValue, selectedCategory]);
 
   return (
     <>
-      {
-        data.map((item) => (
-          <div key={item.id}>
-            <h2>{item.name}</h2>
-            <p>{item.desc}</p>
-          </div>
-        ))
-      }
+      {/* Search */}
+      <input
+        type="text"
+        value={searchValue}
+        placeholder="Search"
+        onChange={(e) => setSearchValue(e.target.value)}
+      />
+
+      {/* Select */}
+      <select
+        value={selectedCategory}
+        onChange={(e) => setSelectedCategory(e.target.value)}
+      >
+        <option value="">All Categories</option>
+        {categories.map((cat, index) => (
+          <option key={index} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+
+      {/* Render */}
+      {filteredData.map((item) => (
+        <div key={item.id}>
+          <h2>{item.name}</h2>
+          <p>{item.desc}</p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
